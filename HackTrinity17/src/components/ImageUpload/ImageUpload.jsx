fa6e1ce9e573e5911df0fa54a2a685432eba5d7e
Audrey <delrosaa@tcd.ie>
2024-10-12 00:55:55 +0100

    Fixes #9 -- completed basic layout

---
+++ HackTrinity17/src/components/ImageUpload/ImageUpload.jsx
@@ -0,0 +1,11 @@
+import styles from './ImageUpload.module.css';
+
+const ImageUpload = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Upload Images</h1>
+    </div>
+  )
+}
+
+export default ImageUpload
