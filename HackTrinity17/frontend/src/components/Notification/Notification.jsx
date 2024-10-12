47d1216ca09b0d64e4fc2e6176133ef6701d1cc5
Audrey <delrosaa@tcd.ie>
2024-10-12 10:38:19 +0100

    FIxes #14 implement user uploads (dockerfile included)

--- HackTrinity17/frontend/src/components/Notification/Notification.jsx
+++ HackTrinity17/frontend/src/components/Notification/Notification.jsx
@@ -1,9 +1,16 @@
 import styles from './Notification.module.css';
+import { MdNotificationsActive } from "react-icons/md";
 
 const Notification = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.notif}>
+        <span>
+          <MdNotificationsActive className={styles.icon} />
+        </span>
         <h1 className={styles.title}>Notification</h1>
+      </div>
+        
     </div>
   )
 }
