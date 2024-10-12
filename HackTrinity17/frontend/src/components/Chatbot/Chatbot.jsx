edc300da23e47a1aa0ce648e2b7c67c55ba457eb
Audrey <delrosaa@tcd.ie>
2024-10-12 11:26:09 +0100

    FIxes #6 Implement template for chatbot

--- HackTrinity17/frontend/src/components/Chatbot/Chatbot.jsx
+++ HackTrinity17/frontend/src/components/Chatbot/Chatbot.jsx
@@ -3,10 +3,13 @@ import styles from './Chatbot.module.css';
 const Chatbot = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Chatbot</h1>
+      <h1 className={styles.title}>Legal Chatbot</h1>
       <div className={styles.msgContainer}>
 
       </div>
+
+
+      
     </div>
   )
 }
