de06f048fe83cade790a6d234edb7adf37b9425e
Zicheng_Liang <liangzicheng02@gmail.com>
2024-10-13 14:28:41 +0100

    final commit

--- frontend/src/components/NextActions/NextActions.jsx
+++ frontend/src/components/NextActions/NextActions.jsx
@@ -1,11 +1,10 @@
 import styles from './NextActions.module.css';
 
-
 const NextActions = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Next Actions</h1>
-      <button className={styles.btn} onClick={handleButtonClick}>
+      <button className={styles.btn}>
           Take action
         </button>
     </div>
